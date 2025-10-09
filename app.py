6b1ce40d7a2c3956a6c4b5f7d79e788a09a63e79
Giuseppe Bellamacina <bellamacina50@gmail.com>
2025-10-09 22:54:49 +0200

    chore: Update TODO comments for next steps

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TODO: se 2 persone lavorano lo stesso giorno, il png non le mostra entrambe (bug nella generazione immagine)
+# TODO: se ci sono 2 persone e 4 stanze, non le distribuisce bene (bug nella logica di assegnazione)
+# TODO: fare un config più bello con multipagina
+# TODO: non fare resettare pagina dopo il download dell'immagine
+# TODO: fare il deploy online
